f7825b7a74622a14205d190d1a5cea7583583ff5
matejpopovski <matej.popovski@gmail.com>
2024-06-26 21:47:39 -0500

    1

--- ChessGame/main.py
+++ ChessGame/main.py
@@ -6,3 +6,15 @@ WIDTH = 1000
 HEIGHT = 900
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 font = pygame.font.Font('freesandbold.ttf', 20)
+big_font = pygame.font.Font('freesandbold.ttf', 50)
+timer = pygame.time.Clock()
+fps = 60
+#game variables and images
+
+run = True
+while run:
+    
+
+
+
+
